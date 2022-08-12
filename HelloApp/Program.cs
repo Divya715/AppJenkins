6347aab71ac7866040615ae2d11b582734dbbb7f
Divya715 <dhivyamarikanti@gmail.com>
2022-08-12 15:52:05 +0530

    New Line

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -11,6 +11,7 @@ namespace HelloApp
             Class1 calc = new Class1();
             Console.WriteLine($"Sum={calc.Add(70, 80)}");
             Console.WriteLine($"Multiply ={calc.Multiply(4, 5)}");
+            Console.WriteLine($"Substrct={calc.Substract(6,3)}");
             Console.ReadLine();
 
         }
