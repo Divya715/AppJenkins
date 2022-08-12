46dfddc617877230c9d6ec84abc6d89c3df2b9e1
Divya715 <dhivyamarikanti@gmail.com>
2022-08-12 14:39:11 +0530

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyCalc;
+namespace HelloApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jenkins");
+            Class1 calc = new Class1();
+            Console.WriteLine($"Sum={calc.Add(70, 80)}");
+            Console.WriteLine($"Multiply ={calc.Multiply(4, 5)}");
+            Console.ReadLine();
+
+        }
+    }
+}
+
